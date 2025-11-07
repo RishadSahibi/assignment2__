c700900c62650fe0836977a525e3651d553a0d22
RishadSahibi <rishad.sahibi@ontariotechu.net>
2025-11-07 03:36:06 -0500

    about page created

---
+++ controllers/about.js
@@ -0,0 +1,9 @@
+const about = (req,res)=>{
+    try {
+        res.render('about')
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+export {about};
--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { home } from "../controllers/home.js";
+import { about } from "../controllers/about.js";
 const route = express.Router();
 
 
 route.get('/',home);
+route.get('/about',about);
 
 
 export default route;
